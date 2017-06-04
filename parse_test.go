15dbecee12e8449dc51bc79134c41b0bc62e9f82
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-06-04 19:59:06 +0530

    Added tests for all Create- functions in parse.go

--- parse_test.go
+++ parse_test.go
@@ -25,3 +25,30 @@ func TestParseStrength(t *testing.T) {
 		t.Errorf("ParseStrength fails")
 	}
 }
+
+func TestCreateAttack(t *testing.T) {
+	attack := CreateAttack("T1 - N - X - 2")
+	if attack.Dir != North {
+		t.Errorf("Error setting direction")
+	}
+
+	if attack.Strength != 2 {
+		t.Errorf("Error setting strength")
+	}
+}
+
+func TestCreateDay(t *testing.T) {
+	day := CreateDay("Day 1$ T1 - S - X - 4: T1 - N - X - 2: T3 - W - X - 3")
+	if len(day.Attacks) != 3 {
+		t.Errorf("Unable to add attacks to day")
+	}
+}
+
+func TestCreateBattle(t *testing.T) {
+	battleInput := "Day 1$ T1 - E - X - 4; Day 2$ T1 - W - X - 3 : T2 - E - X - 3; Day 3$ T3 - N - X - 2: T2 - W - X - 4"
+	battle := CreateBattle(battleInput)
+
+	if len(battle.Days) != 3 {
+		t.Errorf("Unable to add days to battle")
+	}
+}
