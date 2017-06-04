855050b4f1d8d02f87e4410095ebfcb2d23c6f22
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-06-04 18:48:39 +0530

    Defined all the types

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+//A battle consists of several days
+type Battle struct {
+	Days []Day
+}
+
+//Each day has several attacks
+type Day struct {
+	Attacks []Attack
+	N       int
+	E       int
+	S       int
+	W       int
+}
+
+//Alias int to direction to create Direction constants
+type Direction int
+type Attack struct {
+	Dir      Direction
+	Strength int
+}
+
+const (
+	North = iota
+	East
+	South
+	West
+)
+
+//City has a wall
+type City struct {
+	Wall
+}
+
+type Wall struct {
+	Breaches int //Total Number of breaches.
+	N        int //Northern Wall's strength
+	E        int //Eastern
+	S        int //Southern
+	W        int //Western
+}
+
+func main() {
+
+	fmt.Println(West)
+}
