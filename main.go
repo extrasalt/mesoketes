158e2f613131c16ab29ae09059ba5bb8f5a9649f
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-06-07 19:58:28 +0530

    Added a commandline interface that reads input from a file source.txt and prints output to standard out

--- main.go
+++ main.go
@@ -1,6 +1,38 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
+//Reads a file named source.txt and prints output to standard out
 func main() {
+	currentDir, _ := os.Getwd()
+	sourcePath := filepath.Join(currentDir, "/source.txt")
+	source, err := os.Open(sourcePath)
+	defer source.Close()
+	if err != nil {
+		panic(err)
+	}
 
+	bufferedSource := bufio.NewReader(source)
+
+	for {
+		line, err := bufferedSource.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		line = strings.Trim(line, "\n")
+		battle := CreateBattle(line)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(battle.RageOn(&City{}))
+	}
 }
